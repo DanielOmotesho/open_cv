c6acf5890ead1f6ba594fecec20f6e448b7fac44
DanielOmotesho <danielomotesho11@gmail.com>
2026-09-05 10:27:08 +0100

    Add files via upload

--- l2.py
+++ l2.py
@@ -5,6 +5,20 @@ luigi_img=cv2.imread("luigi.png")
 new_image=cv2.resize(luigi_img,(625,625))
 cv2.imwrite("resized_img.png",new_image)
 
+spongebob_img=cv2.imread("spongebob_2.png")
+new_image3=cv2.resize(spongebob_img,(500,500))
+cv2.imwrite("resized_img3.png",new_image3)
+
+bunny_img=cv2.imread("bunny.png")
+new_image4=cv2.resize(bunny_img,(500,500))
+cv2.imwrite("resized_img4.png",new_image4)
+
+
+(rows,cols) = new_image4.shape[:2]
+M = cv2.getRotationMatrix2D((cols/2,rows/2),45,1)
+result = cv2.warpAffine(new_image4,M,(cols,rows))
+cv2.imshow("rotation",result)
+
 
 
 mario_img=cv2.imread("mario.jpg")
@@ -39,9 +53,17 @@ cv2.imshow("bilateral", bilateral)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-border= cv2.imread("spongebob.png")
-borderedImage= cv2.copyMakeBorder(border,10,10,10,10,cv2.BORDER_CONSTANT)
+border= cv2.imread("resized_img3.png")
+borderedImage= cv2.copyMakeBorder(border,10,50,10,50,cv2.BORDER_CONSTANT)
 cv2.imshow("border",borderedImage)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+border2= cv2.imread("resized_img3.png")
+borderedImage = cv2.copyMakeBorder(border2,50,50,50,50,cv2.BORDER_REFLECT,value=1)
+cv2.imshow("border2",borderedImage)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
 
